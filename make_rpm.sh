34f6846837c49b0eb55b8092e27d94cc11702eca
jbo-ads <jbo-ads@laposte.net>
2019-05-09 12:28:06 +0200

    Ajout de mapserver et du patch des ELF

--- make_rpm.sh
+++ make_rpm.sh
@@ -1,21 +1,57 @@
 #!/bin/bash
 
-rm -rf repo
-mkdir repo
-
 rm -rf build
 mkdir build
 cd build
 
-for i in gdal kdu expat proj sqlite ecw tiff
+if [ $# -ne 1 ]
+then
+  printf "$0: Error: need exactly one component name\n" 1>&2
+  exit 1
+fi
+
+name=$1
+
+#
+# Check installation
+#
+idir=~/install/$name
+if [ ! -d $idir ]
+then
+  printf "$0: Error: component \"%s\" has not been found in \"%s/\"\n" \
+                              $name                     $(dirname $idir) 1>&2
+  exit 1
+fi
+
+#
+# Update RPATH/RUNPATH in ELF files so that relative ../lib directory
+# is taken into account
+#
+bins=$(file $(find $idir -type f) | awk -F: '/ELF 64-bit LSB exec/{print$1}')
+libs=$(file $(find $idir -type f) | awk -F: '/ELF 64-bit LSB shared/{print$1}')
+
+for i in $bins
 do
-  cmake3 .. -DNAME=$i
-  cpack3
-  mv *.rpm ../repo
-  rm -rf *
+  rpath=$(patchelf --print-rpath $i)
+  if ! grep -q ORIGIN <<< $rpath
+  then
+    patchelf --set-rpath '$ORIGIN/../lib:'$rpath $i
+  fi
 done
 
-cd ../repo
-createrepo .
-cd ..
-tar czvf repo.tgz repo
+for i in $libs
+do
+  rpath=$(patchelf --print-rpath $i)
+  if ! grep -q ORIGIN <<< $rpath
+  then
+    patchelf --set-rpath '$ORIGIN:'$rpath $i
+  fi
+done
+
+#
+# Pack installation in RPM
+#
+cmake3 -DNAME=$name ..
+cpack3
+
+cp *.rpm ..
